c55543a4b6ee2fcf2473b604f04226792b0573fd
Nitin singh chauhan <89001732+nitinchauhan123@users.noreply.github.com>
2023-12-26 12:18:13 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const name = 'nitin singh'
+let age = 26;
+var city = 'ateli';
+address = 'kanti';
+
+address = 'ateli-mandi';
+age = 27;
+city = 'narnaul';
+let cityCode;
+
+console.table([age,city,address,cityCode])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+// console.log(3 + 6);
+// alert('hello') alert is not defind in node js
+
+// javascript data types names
+//  1 JavaScript Data Types- Primitive
+//  String
+//  Number
+//  Boolean
+//  Undefined
+//  Null
+//  Symbol
+
+// JavaScript Data Types- Non- Primitive
+// Object
+// JavaScript Data Types – Summary
+
+// let name = 'nitin '  string data types;
+
+// let age = 23; Number data types 
+// let islogDeen = true / false boolean data types 
+// let city;  undefined data types
+
+// let village = null; null data types in javascript
+
+// symbol java script in javascript
+
+console.log(typeof 'nitin Singh');
+
+console.log(typeof null)
+
+console.log(typeof undefined)
